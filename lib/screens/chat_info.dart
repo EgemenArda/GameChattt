317e3a4ef77149d7f810a41da454f599e6c841d8
EgemenArda <egemenarda.sezer@stu.bahcesehir.k12.tr>
2023-08-21 15:28:07 +0300

    merhaba

--- lib/screens/chat_info.dart
+++ lib/screens/chat_info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatInfo extends StatefulWidget {
   const ChatInfo({super.key, required this.owner});
@@ -9,14 +10,20 @@ class ChatInfo extends StatefulWidget {
 }
 
 class _ChatInfoState extends State<ChatInfo> {
+
+  User? user = FirebaseAuth.instance.currentUser;
+
   @override
   Widget build(BuildContext context) {
+
+    String username = user!.displayName.toString();
+
     return Scaffold(
       appBar: AppBar(),
       body: Center(
         child: Column(
           children: [
-            Text("Owner: ${widget.owner}"),
+            Text("Owner: ${username}"),
             SizedBox(
               height: 500,
               child: ListView.builder(
